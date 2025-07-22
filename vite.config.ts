5e662c50567aa3659d4fff69dc2dc6f172e3bf83
pederstrand <peder.strand@gmail.com>
2025-07-22 20:33:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
-export default defineConfig({
   base: './',
   plugins: [react()],
   publicDir: 'public'
